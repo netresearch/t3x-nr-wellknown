0f25317277c199d1e97306cfc265e273cc2fa075
Sebastian Mendel <github@sebastianmendel.de>
2026-07-24 20:54:58 +0200

    ci: adopt the Netresearch Build config, scripts and shared code style
    Signed-off-by: Sebastian Mendel <github@sebastianmendel.de>

---
+++ Build/.php-cs-fixer.dist.php
@@ -0,0 +1,10 @@
+<?php
+
+$createConfig = require __DIR__ . '/../.build/vendor/netresearch/typo3-ci-workflows/config/php-cs-fixer/config.php';
+
+return $createConfig(<<<'EOF'
+    This file is part of the package netresearch/nr-wellknown.
+
+    For the full copyright and license information, please read the
+    LICENSE file that was distributed with this source code.
+    EOF, __DIR__ . '/..');
