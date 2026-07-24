830edc9f3bb5b3ef985a94b8cd6c8e73da64e77d
Sebastian Mendel <github@sebastianmendel.de>
2026-07-24 18:00:56 +0200

    feat: scaffold the nr_wellknown extension
    Signed-off-by: Sebastian Mendel <github@sebastianmendel.de>

---
+++ ext_emconf.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of the package netresearch/nr-wellknown.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+$EM_CONF['nr_wellknown'] = [
+    'title'          => 'Netresearch: Well-Known Resources',
+    'description'    => 'Serve the well-known resources a TYPO3 site should provide, from per-site configuration.',
+    'category'       => 'fe',
+    'author'         => 'Team der Netresearch DTT GmbH',
+    'author_email'   => '',
+    'author_company' => 'Netresearch DTT GmbH',
+    'state'          => 'beta',
+    'version'        => '0.1.0',
+    'constraints'    => [
+        'depends'   => [
+            'php'   => '8.2.0-8.5.99',
+            'typo3' => '13.4.0-14.4.99',
+        ],
+        'conflicts' => [],
+        'suggests'  => [],
+    ],
+];
